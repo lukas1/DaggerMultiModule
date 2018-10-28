08613928d74cf9f519f2145d9f608cafd967c01b
Lukáš Vyletel <vyletel@gmail.com>
2018-10-28 12:50:36 +0100

    Create project

---
+++ module/src/main/java/example/com/module/ModuleActivity.kt
@@ -0,0 +1,12 @@
+package example.com.module
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ModuleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module)
+    }
+}
